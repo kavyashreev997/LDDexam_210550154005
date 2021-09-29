a4b32cef4fde501d9551ddaebaec501f61b683ce
kavyashreev997 <88151060+kavyashreev997@users.noreply.github.com>
2021-09-29 15:43:08 +0530

    exam code

---
+++ LDD_exam/LDD_exam/write1.c
@@ -0,0 +1,30 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main()
+{
+	int fd;
+	char u_buff[]="This message is from user to kernel";
+	char k_buff[100];
+
+	fd=open("/dev/dev1",O_RDWR,0777);
+	if(fd<0)
+	{
+		printf("\nError opening file\n");
+		exit(1);
+	}
+	
+	write(fd,u_buff,sizeof(u_buff));
+	//read(fd,k_buff,50);
+
+	printf("data read from kernel : %s\n",k_buff);	
+	close(fd);
+
+
+	return 0;
+}
+
